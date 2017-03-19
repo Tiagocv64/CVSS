b7b9aaccd8b8b68448339f8f13a4c8927b61616f
TIAGOCV-PC\tcver <tcverdade.64@gmail.com>
2017-03-19 18:05:58 +0000

    Login adicionado

--- app/src/main/java/com/creation/tiagocv/cvss/LoginActivity.java
+++ app/src/main/java/com/creation/tiagocv/cvss/LoginActivity.java
@@ -1,13 +1,88 @@
 package com.creation.tiagocv.cvss;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
+    // Choose an arbitrary request code value
+    private static final int RC_SIGN_IN = 6464;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+            Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
+            LoginActivity.this.startActivity(myIntent);
+            finish();
+        } else {
+            // not signed in
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                                    new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()))
+                            .setLogo(R.drawable.cvss)
+                            .setTheme(R.style.CVSSTheme)
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+            if (requestCode == RC_SIGN_IN) {
+                IdpResponse response = IdpResponse.fromResultIntent(data);
+
+                // Successfully signed in
+                if (resultCode == ResultCodes.OK) {
+                    Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    LoginActivity.this.startActivity(myIntent);
+                    finish();
+                    return;
+                } else {
+                    // Sign in failed
+                    if (response == null) {
+                        // User pressed back button
+                        showSnackbar(getResources().getString(R.string.sign_in_cancelled));
+                        return;
+                    }
+
+                    if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                        showSnackbar(getResources().getString(R.string.no_internet_connection));
+                        return;
+                    }
+
+                    if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                        showSnackbar(getResources().getString(R.string.unknown_error));
+                        return;
+                    }
+                }
+
+                showSnackbar(getResources().getString(R.string.unknown_sign_in_response));
+            }
+        }
+
+
+    void showSnackbar(String messageShow){
+        View parentLayout = findViewById(R.id.activity_login);
+        Snackbar.make(parentLayout, messageShow, Snackbar.LENGTH_LONG)
+                .setActionTextColor(getResources().getColor(android.R.color.holo_red_light ))
+                .show();
     }
 }
---
+++ app/src/main/java/com/creation/tiagocv/cvss/MainActivity.java
@@ -0,0 +1,55 @@
+package com.creation.tiagocv.cvss;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button signOutButton = (Button) findViewById(R.id.sign_out);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.getId() == R.id.sign_out) {
+                    AuthUI.getInstance()
+                            .signOut(MainActivity.this)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    // user is now signed out
+                                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                                    finish();
+                                }
+                            });
+                }
+            }
+        });
+
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.sign_out) {
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        public void onComplete(@NonNull Task<Void> task) {
+                            // user is now signed out
+                            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                            finish();
+                        }
+                    });
+        }
+    }
+
+}
